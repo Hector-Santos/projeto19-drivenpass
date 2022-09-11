6dbf97318bdf5208e01e6ad7ea096e8fba7d68a8
Hector-Santos <habs.404@gmail.com>
2022-09-11 02:45:09 -0300

    adicionadas rotas de criação do usuário, login e criação de credenciais

--- src/routes/credentialsRouter.ts
+++ src/routes/credentialsRouter.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import { createCredential } from '../controllers/credentialsController.js';
 import { validate } from '../middlewares/validationMiddleware.js';
 import { credentialsSchema } from '../schemas/credentialsSchema.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const credentialsRouter = express.Router();
-credentialsRouter.post("/createcredential", (req, res, next) => validate(req, res, next, credentialsSchema), createCredential);
+credentialsRouter.post("/createcredential", (req, res, next) => validate(req, res, next, credentialsSchema), verifyToken, createCredential);
 
 
 export default credentialsRouter
--- src/services/credentialsService.ts
+++ src/services/credentialsService.ts
@@ -1,15 +1,18 @@
 import { insert, findByTagAndId } from "../repositories/credentialsRepository.js";
 import { ICredentialData } from "../types/credentialsTypes.js";
-import bcrypt from 'bcrypt'
-import  jwt  from "jsonwebtoken";
+import Cryptr from "cryptr";
 import dotenv from 'dotenv'
 import { findById } from "../repositories/usersRepository.js";
 
 dotenv.config()
 
+const ENCRYPTION_KEY:string  = process.env.ENCRYPTION_KEY || "key"
+
+const cryptr = new Cryptr(ENCRYPTION_KEY)
+
 export async function createCredential(credentialData: ICredentialData){
   
-  const passwordHash = bcrypt.hashSync(credentialData.password, 10);
+  const encryptedPassword = cryptr.encrypt(credentialData.password)
 
   const user = await findById(credentialData.userId)
 	if(!user) throw {type: 'not_found', message: 'a user with the provided id could not be found'}
@@ -22,7 +25,7 @@ export async function createCredential(credentialData: ICredentialData){
     userId: credentialData.userId,
     url: credentialData.url,
     userName:credentialData.userName,
-    password:passwordHash,
+    password:encryptedPassword,
     tag:credentialData.tag
    }
   await insert(encryptedCredentialData)
